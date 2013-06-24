b9a18de3f58aa4d54393ab8e3dc4e16f281c3f24
matt <matt@madeye.com>
2013-06-24 07:37:53 +0100

    First attempt at webapp

--- src/com/madeye/clojure/ampache/top100.clj
+++ src/com/madeye/clojure/ampache/top100.clj
@@ -9,3 +9,12 @@
 (require '[com.madeye.clojure.common.common :as c])
 
 (defn reload [] (use :reload-all 'com.madeye.clojure.ampache.top100))
+
+(use '[ring.adapter.jetty :only (run-jetty)])
+
+(defn app
+  [{:keys [uri]}]
+  {:body (format "You requested %s" uri)})
+
+(def server (run-jetty #'app {:port 8080 :join? false}))
+
