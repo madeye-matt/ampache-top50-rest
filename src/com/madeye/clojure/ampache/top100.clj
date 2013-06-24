f3d62b2437b9727a21d84ab234c8813eda273576
matt <matt@madeye.com>
2013-06-24 07:50:56 +0100

    Added compojure

--- src/com/madeye/clojure/ampache/top100.clj
+++ src/com/madeye/clojure/ampache/top100.clj
@@ -1,4 +1,7 @@
 (ns com.madeye.clojure.ampache.top100
+  (:use [compojure.core :only (GET PUT POST defroutes)])
+  (:require (compojure handler route)
+            [ring.util.response :as response])
   (:gen-class))
 
 (require '[clojure.string :as str])
@@ -6,15 +9,23 @@
 (require '[clj-time.core :as tm])
 (require '[clj-time.local :as tloc])
 (require '[clj-time.format :as tfmt])
+
 (require '[com.madeye.clojure.common.common :as c])
+(require '[com.madeye.clojure.ampache.ampachedb :as adb])
 
 (defn reload [] (use :reload-all 'com.madeye.clojure.ampache.top100))
 
-(use '[ring.adapter.jetty :only (run-jetty)])
+;(defn app
+;  [{:keys [uri]}]
+;  {:body (format "You requested %s" uri)})
 
-(defn app
-  [{:keys [uri]}]
-  {:body (format "You requested %s" uri)})
+;(def server (run-jetty #'app {:port 8080 :join? false}))
 
-(def server (run-jetty #'app {:port 8080 :join? false}))
+(defroutes app*
+    (GET "/" request "Welcome!")
+)
+
+(def app (compojure.handler/api app*))
+
+(defn startserver [] (run-jetty #'app {:port 8080 :join? false}))
 
