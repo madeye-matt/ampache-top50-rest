95e048017325ec902568414fa367f083d8df6fe8
matt <matt@madeye.com>
2013-06-25 08:11:43 +0100

    Implemented query param to set size of result set

--- src/com/madeye/clojure/ampache/top100.clj
+++ src/com/madeye/clojure/ampache/top100.clj
@@ -11,24 +11,36 @@
 (require '[clj-time.format :as tfmt]) 
 
 (require '[ring.adapter.jetty :as jetty :only (run-jetty)])
+; (require '[ring.middleware.params :as params :only (wrap-params)])
 (require '[clojure.data.json :as json])
 
 (require '[com.madeye.clojure.common.common :as c])
 (require '[com.madeye.clojure.ampache.ampachedb :as adb])
 
+(declare default-top server-port)
+
 (defn reload [] (use :reload-all 'com.madeye.clojure.ampache.top100))
 
 (defn top-tracks
   "Default 'top tracks' function"
-  []
+  [num-results]
   { :status 200
-    :body (json/write-str (adb/top-result (c/get-date-range :last_month) adb/group-song 10))
+    :body (json/write-str (adb/top-result (c/get-date-range :last_month) adb/group-song num-results))
   }
+)
+
+(defn get-num-results
+  "Gets number of results query map (assumes a string) or returns default top"
+  [params]
+  (if-let [numResults (params "num" )]
+    (read-string numResults)
+    default-top
+  )
 )
 
 (defroutes app*
     (GET "/" request "Welcome!")
-    (GET "/top-tracks" [] (top-tracks))
+    (GET "/song-play" {params :query-params} (top-tracks (get-num-results params)))
 )
 
 (def app (compojure.handler/api app*))
@@ -40,6 +52,7 @@
   [config-file]
   (def config (c/load-props config-file))
   (def server-port (read-string (config :server-port)))
+  (def default-top (read-string (config :default-top)))
   (adb/initialise config-file)
   (startserver server-port)
 )
