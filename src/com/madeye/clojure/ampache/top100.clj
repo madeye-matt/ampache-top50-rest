8d5fa2bb0d47c3d87035a41034193c6ff40cddb0
matt <matt@madeye.com>
2013-06-25 08:25:08 +0100

    Added facility to get plays by album and artist

--- src/com/madeye/clojure/ampache/top100.clj
+++ src/com/madeye/clojure/ampache/top100.clj
@@ -21,11 +21,11 @@
 
 (defn reload [] (use :reload-all 'com.madeye.clojure.ampache.top100))
 
-(defn top-tracks
+(defn top-results
   "Default 'top tracks' function"
-  [num-results]
+  [group-fn date-range num-results]
   { :status 200
-    :body (json/write-str (adb/top-result (c/get-date-range :last_month) adb/group-song num-results))
+    :body (json/write-str (adb/top-result date-range group-fn num-results))
   }
 )
 
@@ -38,9 +38,26 @@
   )
 )
 
+(defn get-date-range
+  "Gets a date range from either the 'period' parameter (last-week last-month or ever) or the 'start' and 'end' parameters"
+  [params]
+  (case (params "period")
+    "last-week"
+      (c/get-date-range :last_week)
+    "last-month"
+      (c/get-date-range :last_month)
+    "ever"
+      (c/get-date-range :all_time)
+    nil
+      (c/get-date-range :last_month)
+  )
+)
+
 (defroutes app*
     (GET "/" request "Welcome!")
-    (GET "/song-play" {params :query-params} (top-tracks (get-num-results params)))
+    (GET "/song-play" {params :query-params} (top-results adb/group-song (get-date-range params) (get-num-results params)))
+    (GET "/album-play" {params :query-params} (top-results adb/group-album (get-date-range params) (get-num-results params)))
+    (GET "/artist-play" {params :query-params} (top-results adb/group-artist (get-date-range params) (get-num-results params)))
 )
 
 (def app (compojure.handler/api app*))
